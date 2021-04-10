256fe48fcec936b6f30d999b03837f864d628a8a
Luciano Borges <60045401+Luciano-Arvelino-Borges@users.noreply.github.com>
2021-04-10 19:10:47 -0300

    Initial commit

---
+++ app/src/main/java/com/example/projetointegrador/Activity/Login.java
@@ -0,0 +1,16 @@
+package com.example.projetointegrador.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projetointegrador.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
