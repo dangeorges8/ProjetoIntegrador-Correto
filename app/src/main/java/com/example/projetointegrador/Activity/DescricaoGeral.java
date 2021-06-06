905c34e2ebf016df7c2eae4a20a56e8fdd5ba2f2
Daniel Georges <dannnn.georges@gmail.com>
2021-06-06 10:09:19 -0300

    Navegação entre a página de alertas e descrição

---
+++ app/src/main/java/com/example/projetointegrador/Activity/DescricaoGeral.java
@@ -0,0 +1,17 @@
+package com.example.projetointegrador.Activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.projetointegrador.R;
+
+public class DescricaoGeral extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.descricao_alerta_geral);
+    }
+}
--- app/src/main/java/com/example/projetointegrador/Activity/SendAlerts.java
+++ app/src/main/java/com/example/projetointegrador/Activity/SendAlerts.java
@@ -1,6 +1,10 @@
 package com.example.projetointegrador.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +13,62 @@ import com.example.projetointegrador.R;
 
 public class SendAlerts extends AppCompatActivity {
 
+    private ImageButton buttonBuraco;
+    private ImageButton buttonIluminacao;
+    private ImageButton buttonOnibus;
+    private ImageButton buttonAcidente;
+    private ImageButton buttonBloqueio;
+    private ImageButton buttonPreco;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.send_alerts);
+
+        final View.OnClickListener onClickListenerGeral = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAlertaGeralActivity();
+            }
+        };
+
+        buttonBuraco = (ImageButton) findViewById(R.id.imageButton);
+        buttonIluminacao = (ImageButton) findViewById(R.id.imageButton2);
+        buttonAcidente = (ImageButton) findViewById(R.id.imageButton7);
+        buttonBloqueio = (ImageButton) findViewById(R.id.imageButton8);
+        buttonOnibus = (ImageButton) findViewById(R.id.imageButton5);
+        buttonPreco = (ImageButton) findViewById(R.id.imageButton9);
+
+        buttonBuraco.setOnClickListener(onClickListenerGeral);
+        buttonIluminacao.setOnClickListener(onClickListenerGeral);
+        buttonAcidente.setOnClickListener(onClickListenerGeral);
+        buttonBloqueio.setOnClickListener(onClickListenerGeral);
+        buttonOnibus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAlertaOnibusActivity();
+            }
+        });
+        buttonPreco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAlertaPrecoActivity();
+            }
+        });
+    }
+
+    private void openAlertaPrecoActivity() {
+        Intent intent = new Intent(this, DescricaoPreco.class);
+        startActivity(intent);
+    }
+
+    public void openAlertaOnibusActivity() {
+        Intent intent = new Intent(this, DescricaoOnibus.class);
+        startActivity(intent);
+    }
+
+    public void openAlertaGeralActivity() {
+        Intent intent = new Intent(this, DescricaoGeral.class);
+        startActivity(intent);
     }
 }
